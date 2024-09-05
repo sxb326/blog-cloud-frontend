dfba8517df5b61ab910657076bb06d9c6e4f7011
sxb <sxb0326@qq.com>
2024-09-05 13:11:46 +0800

    私信

--- src/components/message/Chat.vue
+++ src/components/message/Chat.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="message-content">
-        <el-container>
-            <el-aside class="container left">
+        <el-row class="container">
+            <el-col :span="6" style="border-right: 1px solid gainsboro">
                 <div class="search-div">
                     <el-input v-model="keyword" style="width: 280px" placeholder="搜索联系人">
                         <template #append>
@@ -10,9 +10,9 @@
                     </el-input>
                 </div>
                 <div v-loading="loading" class="list-div">
-                    <el-row v-for="item in list" :key="item.uid" class="list-item"
-                        @click="clickContact(item.contactUid)"
-                        :style="{ background: item.contactUid === chat.contactUid ? '#d1ffff' : 'white' }">
+                    <el-row v-for="item in conversationList" :key="item.uid" class="list-item"
+                        :style="{ background: item.contactUid === chat.contactUid ? '#d1ffff' : 'white' }"
+                        @click="clickConversation(item.contactUid)">
                         <el-col :span="6">
                             <el-avatar :size="40" :src="imgUrl + item.picUid" />
                         </el-col>
@@ -22,94 +22,63 @@
                         </el-col>
                     </el-row>
                 </div>
-            </el-aside>
-            <div class="right">
-                <div>
-                    <div ref="contentRef" class="chat-content">
-                        <div v-for="item in contentList" :key="item.uid" style="width: 100%;">
-                            <div v-if="item.content.length > 0" style="max-width: 80%;"
-                                :style="item.isSend ? { 'float': 'right' } : { 'float': 'left' }"
-                                class="chat-content-item">
-                                <div class="chat-content-title"
-                                    :style="item.isSend ? { 'text-align': 'right' } : { 'text-align': 'left' }">
-                                    {{ item.createTime }}</div>
-                                <div class="chat-content-message"
-                                    :style="item.isSend ? { 'background': '#b5ffff' } : { 'background': 'gainsboro' }">
-                                    <div>{{ item.content }}</div>
-                                </div>
+            </el-col>
+            <el-col :span="18">
+                <div ref="chatRef" @scroll="onScroll" class="chat-content">
+                    <div v-for="item in chatList" :key="item.uid">
+                        <div v-if="item.content.length > 0" style="max-width: 60%;"
+                            :style="item.isSend ? { 'float': 'right' } : { 'float': 'left' }" class="chat-content-item">
+                            <div class="chat-content-title"
+                                :style="item.isSend ? { 'text-align': 'right' } : { 'text-align': 'left' }">
+                                {{ item.createTime }}</div>
+                            <div class="chat-content-message"
+                                :style="item.isSend ? { 'background': '#b5ffff' } : { 'background': 'gainsboro' }">
+                                <div>{{ item.content }}</div>
                             </div>
                         </div>
                     </div>
-                    <div class="chat-input">
-                        <el-form class="commentDiv" ref="formRef" :model="chat" :rules="rules">
-                            <el-form-item prop="content">
-                                <el-input v-model="chat.content" :rows="4" type="textarea" maxlength="1000"
-                                    show-word-limit />
-                            </el-form-item>
-                            <el-button size="small" type="primary" style="float: right" @click="send">发送</el-button>
-                        </el-form>
-                    </div>
                 </div>
-            </div>
-        </el-container>
+                <div class="chat-input">
+                    <el-form class="commentDiv" ref="formRef" :model="chat" :rules="rules">
+                        <el-form-item prop="content">
+                            <el-input v-model="chat.content" :rows="4" type="textarea" maxlength="1000"
+                                show-word-limit />
+                        </el-form-item>
+                        <el-button size="small" type="primary" style="float: right" @click="send">发送</el-button>
+                    </el-form>
+                </div>
+            </el-col>
+        </el-row>
     </div>
 </template>
 <script setup>
 import { nextTick, onMounted, reactive, ref } from 'vue';
 import request from '@/utils/request.js';
-import { useRoute } from 'vue-router';
 import { Search } from '@element-plus/icons-vue';
 
-const route = useRoute();
 const imgUrl = import.meta.env.VITE_IMG_URL;
 let loading = ref(false)
 
-let list = ref([]);
+let conversationList = ref([]);
 
 let keyword = ref('')
 
 const doSearch = () => {
-    list.value = []
-    getList()
+    conversationList.value = []
+    getConversationList()
 }
 
-const getList = () => {
+const getConversationList = () => {
     loading.value = true;
-    request.get('/message/chat/listContact', { params: { keyword: keyword.value } }).then((result) => {
-        list.value = result.data;
+    request.get('/message/conversation/list', { params: { keyword: keyword.value } }).then((result) => {
+        conversationList.value = result.data;
         loading.value = false
     });
 }
 
-let interval;
-
-const clickContact = (contactUid) => {
-    clearInterval(interval)
-    if (chat.contactUid !== contactUid) {
-        contentList.value = []
-        chat.contactUid = contactUid
-        chat.content = ''
-        getData('1')
-        interval = setInterval(() => {
-            getData('2')
-        }, 1000);
-    }
-
-}
-
-let contentList = ref([])
-const contentRef = ref(null)
-
-const getData = (type) => {
-    request.get('/message/chat/listContent', { params: { contactUid: chat.contactUid } }).then(result => {
-        contentList.value = result.data
-        if (type === '1') {
-            nextTick(() => {
-                contentRef.value.scrollTop = contentRef.value.scrollHeight;
-            });
-        }
-    })
-}
+let chatList = ref([])
+const chatRef = ref(null)
+let cursor = ref(0)
 
 let chat = reactive({ contactUid: '', content: '' })
 const formRef = ref(null);
@@ -117,25 +86,62 @@ const rules = reactive({
     content: [{ required: true, message: '请输入内容', trigger: 'blur' }]
 })
 
+const clickConversation = (contactUid) => {
+    if (chat.contactUid !== contactUid) {
+        chatList.value = []
+        chat.contactUid = contactUid
+        chat.content = ''
+        getChatList()
+    }
+}
+
+const getChatList = () => {
+    request.get('/message/chat/list', { params: { contactUid: chat.contactUid, cursor: cursor.value } }).then(result => {
+        chatList.value.unshift(...result.data)
+        cursor.value = result.data[0].uid
+        nextTick(() => {
+            chatRef.value.scrollTop = chatRef.value.scrollHeight;
+        });
+    })
+}
+
+const onScroll = () => {
+    if (chatRef.value) {
+        const div = chatRef.value;
+        if (div.scrollTop <= 0 || div.scrollTop <= div.offsetHeight - div.clientHeight) {
+            refreshChatList()
+        }
+    }
+};
+
+const refreshChatList = () => {
+    request.get('/message/chat/list', { params: { contactUid: chat.contactUid, cursor: cursor.value } }).then(result => {
+        chatList.value.unshift(...result.data)
+        cursor.value = result.data[0].uid
+    })
+}
+
+
 const send = () => {
     formRef.value.validate((valid) => {
         if (!valid) {
             return false;
         }
         request.post('/message/chat/send', chat).then(() => {
-            contentList.value = []
+            chatList.value = []
             chat.content = ''
-            getData('1')
+            getChatList()
         })
     })
 }
+
 onMounted(() => {
-    getList();
-    let id = route.query.id;
-    if (id) {
-        chat.contactUid = id;
-        getData('1')
-    }
+    getConversationList();
+    // let id = route.query.id;
+    // if (id) {
+    //     chat.contactUid = id;
+    //     getChatList()
+    // }
 });
 </script>
 
@@ -149,13 +155,12 @@ onMounted(() => {
 }
 
 .left {
-    width: 25%;
-    border-right: 1px solid gainsboro;
+    /* border-right: 1px solid gainsboro; */
 }
 
 .right {
     width: 100%;
-    margin: 10px;
+    /* margin: 10px; */
 }
 
 .search-div {
@@ -194,6 +199,8 @@ onMounted(() => {
     overflow: auto;
     display: flex;
     flex-direction: column;
+    width: 100%;
+    /* margin:10px */
 }
 
 .chat-content-item {
@@ -212,9 +219,11 @@ onMounted(() => {
 
 .chat-content-message div {
     margin: 5px;
+    word-wrap: break-word;
 }
 
 .chat-input {
     height: calc(20vh);
+    margin: 10px;
 }
 </style>
